a032494a3bc2f51839efe5e77ce6d4483b445482
Fern <stfern14@gmail.com>
2025-05-05 21:05:22 +1200

    add project travel-gallery

--- src/components/Hero/Hero-en.jsx
+++ src/components/Hero/Hero-en.jsx
@@ -2,7 +2,7 @@ export default {
     hero: {
       title: "Hi, I'm Fern",
       description:
-        "Developer with 2.5 years of experience in IT support, automation, and software development. Skilled in both web technologies and process improvement, and open to diverse opportunities.",
+        "A developer with 2.5 years of experience in IT support, RPA automation (UiPath), and web development. Skilled in React and process improvement. Open to opportunities in Frontend, Full Stack, or RPA development where I can grow and contribute to meaningful projects.",
       contact: "Contact me",
       downloadResume: "Download Resume",
       downloadCV: "Download CV"
